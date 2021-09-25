3e2ec17c5397b13503b46810e17563d295d4df91
vaibhav0311 <sharma.vaibhav0311@gmail.com>
2021-09-25 23:50:09 +0530

    added buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -16,20 +17,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var rnum = 1;
+  var lnum = 1;
+
+  void run() {
+    rnum = 3;
+    lnum = 4;
+    Random(rnum);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(
-            child: Image(
-              image: AssetImage("images/dice1.png"),
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  run();
+                });
+                print('Left button was pressed!');
+              },
+              child: Image(
+                image: AssetImage("images/dice$lnum.png"),
+              ),
             ),
           ),
           Expanded(
-            child: Image(
-              image: AssetImage("images/dice6.png"),
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  run();
+                });
+                print('Right button was pressed!');
+              },
+              child: Image(
+                image: AssetImage("images/dice$rnum.png"),
+              ),
             ),
           ),
         ],
